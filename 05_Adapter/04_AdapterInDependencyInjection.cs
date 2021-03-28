38affbe4d2115ac6f03ef4e15ff60e390a6206d2
vicavila <victoral.avila@gmail.com>
2021-03-28 09:38:15 -0500

    comment on adapter

--- 05_Adapter/04_AdapterInDependencyInjection.cs
+++ 05_Adapter/04_AdapterInDependencyInjection.cs
@@ -7,6 +7,7 @@ using Autofac;
 using Autofac.Features.Metadata;
 using static System.Console;
 
+// how autofac helps you build adapters and inject them correctly too
 namespace Adapter.AdapterInDependencyInjection
 {
  public interface ICommand
@@ -66,18 +67,27 @@ namespace Adapter.AdapterInDependencyInjection
   public static void Run()
   {
    var b = new ContainerBuilder();
+   // the WithMetadata method is added after including the name
+   // attribute on Button, it's to send additional info
+   // when registering the adapter
    b.RegisterType<SaveCommand>().As<ICommand>().WithMetadata("Name", "Save");
    b.RegisterType<OpenCommand>().As<ICommand>().WithMetadata("Name", "Open");
+   // this way only one button will be registered
    //b.RegisterType<Button>();
+   // with this one the two buttons will be registered because we are specifying
+   // the interface to bring the two buttons from the two interface implementations
    //b.RegisterAdapter<ICommand, Button>(cmd => new Button(cmd));
+   // here we have the same but passing some metadata defined before
    b.RegisterAdapter<Meta<ICommand>, Button>(cmd => new Button(cmd.Value, (string)cmd.Metadata["Name"]));
    b.RegisterType<Editor>();
    using (var c = b.Build())
    {
     var editor = c.Resolve<Editor>();
+    // this one is just to demosntrate that all the buttons were registered
     //editor.ClickAll();
     foreach (var btn in editor.Buttons)
     {
+     // here we demonstrate that the name is passed on through the metadata
      btn.PrintMe();
     }
    }
